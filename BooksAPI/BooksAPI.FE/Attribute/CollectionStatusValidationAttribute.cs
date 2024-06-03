f22abcd9397d9dad386ae9ab5bb3276af0835111
Exonault <kristiankrachmarov@gmail.com>
2024-06-03 15:09:52 +0300

    usermanga document minor fixing and ui improvements

--- BooksAPI/BooksAPI.FE/Attribute/CollectionStatusValidationAttribute.cs
+++ BooksAPI/BooksAPI.FE/Attribute/CollectionStatusValidationAttribute.cs
@@ -17,7 +17,7 @@ public class CollectionStatusValidationAttribute : ValidationAttribute
         if (collectionStatus == UserMangaConstants.CollectingStatus.Collected &&
             libraryManga!.PublishingStatus == LibraryMangaConstants.PublishingType.Publishing)
         {
-            return new ValidationResult("Cannot collect a manga that is still publishing.");
+            return new ValidationResult("You cant have a collected manga that is still publishing");
         }
 
         if (collectionStatus == UserMangaConstants.CollectingStatus.Collected &&
